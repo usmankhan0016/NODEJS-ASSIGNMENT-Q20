95ed73b98e8e1a3aca34de395e69c3ac08b2ab26
Usman Khan <160907897+usmankhan0016@users.noreply.github.com>
2024-04-05 05:04:20 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+// EXERCISE 20:
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+let cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Multan"];
+for (let i = 0; i < cities.length; i++) {
+    console.log(cities[i]);
+}
---
+++ app.ts
@@ -0,0 +1,8 @@
+// EXERCISE 20:
+
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let cities:string[]=["Karachi","Lahore","Islamabad","Quetta","Multan"];
+for(let i=0; i<cities.length;i++){
+    console.log(cities[i]);
+}
